7ac9297bf5f94e6e91b0f0f7d67deffe765f30f7
Sai Ragavan <sai.ragavan412@gmail.com>
2025-06-19 16:47:03 +0200

    I added Smileys for a nicer look

--- SmartClock_Project/src/main.cpp
+++ SmartClock_Project/src/main.cpp
@@ -7,6 +7,50 @@
 SparkFun_ENS160 ens160;
 BME280 bme280;
 SerLCD lcd;
+int ppm;
+int tempDiff = 4;
+float temp;
+
+byte smiley[8] = {
+    0b00000,
+    0b00000,
+    0b01010,
+    0b00000,
+    0b00000,
+    0b10001,
+    0b01110,
+    0b00000};
+
+byte neutral[8] = {
+    0b00000,
+    0b00000,
+    0b01010,
+    0b00000,
+    0b00000,
+    0b11111,
+    0b00000,
+    0b00000};
+
+byte frownie[8] = {
+    0b00000,
+    0b00000,
+    0b01010,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b01110,
+    0b10001};
+
+byte tot[8] = {
+    0b00000,
+    0b00000,
+    0b10101, // X   X  (Augen)
+    0b00000,
+    0b00000,
+    0b00000,
+    0b01110, // Mund
+    0b10001  // Wangen/Umriss
+};
 
 void setup()
 {
@@ -39,25 +83,56 @@ void setup()
   lcd.setContrast(50);
 
   lcd.createChar(0, smiley);
-  lcd.createChar(1, frownie);
+  lcd.createChar(1, neutral);
+  lcd.createChar(2, frownie);
+  lcd.createChar(3, tot);
 }
 
 void printTemp()
 {
+  lcd.setCursor(0, 1);
   lcd.print("Temp: ");
-  lcd.print(temp - diff, 2);
+  lcd.print(temp, 2);
   lcd.println(" C");
-
   delay(200);
+}
+
+void printCO2()
+{
+  lcd.setCursor(0, 0);
+  if (ppm > 1000)
+  {
+
+    lcd.print("CO2 : ");
+  }
+  else if (ppm <= 1000)
+  {
+    lcd.print("CO2 :  ");
+  }
+
+  lcd.print(ppm);
+  lcd.print(" ppm");
+  lcd.setCursor(15, 0);
+  if (ppm <= 400)
+  {
+    lcd.writeChar(0);
+  }
+  else if (ppm <= 1000)
+  {
+    lcd.writeChar(1);
+  }
+  else if (ppm > 1000)
+  {
+    lcd.writeChar(2);
+  }
 }
 
 void loop()
 {
-  lcd.print("CO2 : ");
-  lcd.print(ens160.getECO2());
-  lcd.println(" ppm");
-  lcd.print("Temp: ");
-  lcd.print(bme280.readTempC(), 2);
-  lcd.println(" C");
+  ppm = ens160.getECO2();
+  temp = bme280.readTempC() - tempDiff;
+
+  printCO2();
+  printTemp();
   delay(200);
 }
